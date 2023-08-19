923092ee1f00c2e575587620d5848ffc21c6481e
Pankaj Kumar <137532694+pythonwithpankaj@users.noreply.github.com>
2023-08-19 11:08:19 +0530

    flow control type
    flow control #if block , #if Elif block

---
+++ flow_control.py
@@ -0,0 +1,167 @@
+###Flow control
+         #if Block
+         #if else block
+         #if elif else block
+         #Nested if block   
+
+# n=int(input("enter a number:"))
+# if n>=0:
+#     print(n,"is positive")
+# else:
+#     print(n,"is negative")    
+# if n%2==0:
+#       print(n,"is even")
+# else:
+#       print(n,"is odd")      
+
+# n=int(input("enter a number:"))      #1-100
+# if n>=1 and n<=100:
+#       print(n,"is in range 1-100")
+# else:
+#       print(n,"is not in range 1-100")
+# if n in range(1,100):
+#       print(n,"is in range 1-100")
+# else:
+#       print(n,"is in range 1-100")  
+
+
+# user="abcd"
+# password="1234"
+ 
+# username=input("enter username:")
+# pswd=input("enter password:")
+ 
+# if username==user and password==pswd:
+#     print("login successful")
+# else:
+#     print("login failed")
+
+# print(eval("5*10"))
+
+### Weather classification
+
+ #<=0 -- freezzing weather
+ #>0 and <=10 -- very cold weather
+ #>10 and <=20 -- cold weather
+ #>20 and <=30 -- normal weather
+ #>30 and <=40 -- hot
+ #>40 and <=50 -- very hot
+ #>50          -- Buring weather
+
+#temp=float(input("enter temprature:"))
+
+       #  if temp<=0:
+       #      print("freezzing weather")
+       #  elif temp>0 and temp<=10:
+       #      print("very cold weather")
+       #  elif temp>10 and temp<=20:
+       #      print("cold weather")
+       #  elif temp>20 and temp<=30:
+       #      print("Normal weather")
+       #  elif temp>30 and temp<=40:
+       #      print("Hot weather")
+       #  elif temp>40 and temp<=50:
+       #      print("very Hot weather")
+       #  else:
+       #      print("Burring weather")                        
+
+
+###division check
+
+# >0 and <=33 -- fail
+# >33 and <=40 -- third division 
+# >40 and <=60 -- second division
+# >60 and <=100 -- first division
+
+      #  per=float(input("enter division"))
+        
+      #  if per>0 and per<=33:
+      #      print("fail")
+      #  elif per>33 and per<=40:
+      #      print("third division")
+      #  elif per>40 and per<=60:
+      #      print("second division")
+      #  elif per>60 and per<=100:
+      #      print("first division")
+      #  else:
+      #      print("invalid division value")
+       
+
+
+   #  n1=int(input("enter number 1:"))
+   #  n2=int(input("enter number 2:"))
+   #  n3=int(input("enter number 3:"))
+     
+   #  if n1>n2 and n1>n3:
+   #      print(n1,"is largest")
+   #  elif n2>n1 and n2>n3:
+   #      print(n2,"is largest")
+   #  else:
+   #      print(n3,"is largest")  
+
+
+
+##Nested if
+
+   # n=int(input("enter number:"))
+    
+   # if n>=0:
+   #     if n%2==0:
+   #         print("Even")
+   #     else:
+   #         print("odd") 
+   # else:
+   #     print("no is negative")     
+
+
+
+#  ch=input("enter a character:")
+ 
+#  if ch>='a' and ch<='z' or ch>='A' and ch<='Z':
+#      print(ch,"is an alphabet")
+#  elif ch>='0' and ch<='9':
+#      print(ch,"is an digit")
+#  elif ch=='!' or ch=='@' or ch=='$' or ch=='_':
+#      print(ch,"is a special character")
+#  else:
+#      print("invalid input")  
+
+
+# a=int(input("a="))
+# b=int(input("b="))
+# c=int(input("c="))
+
+#  d=(b*2)-(4*a*c)
+ 
+#  if d>=0:
+#      print("roots are real")
+#  else:
+#      print("roots are imaginary")   
+
+
+
+
+#   n1=int(input("enter 1st number:"))
+#   n2=int(input("enter 2nd number:"))
+   ###  i=int(input("enter 1,2,3,4 where\n1. addition\n2. subtraction\n3. multiplication\n4. division"))
+
+#   i=input("enter operation + - * /:")
+#   if i=='+':
+#       print("addition",n1+n2)
+#   elif i=='-':
+#       print("subtraction",n1-n2)
+#   elif i=='*':
+#       print("multiplication",n1*n2)
+#   elif i=='/':
+#       print("division",n1/n2)
+#   else:
+#       print("invalid operation")                
+
+
+#  n1=int(input("press 1 for Hindi\npress 2 for English:"))
+#  if n1==1:
+#      print("Hindi language is selected")
+#  elif n1==2:
+#      print("English language is selected")
+#  else:
+#      print("invalid selected")        
